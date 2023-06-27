069c114188d061ca156121782ff695ad1a55009d
Kevin Stout <stoutkev3@gmail.com>
2023-06-26 22:18:36 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+
+// GET endpoint
+app.get("/api/data", (req, res) => {
+  // Handle GET request here
+  res.send("This is the GET endpoint");
+});
+
+// POST endpoint
+app.post("/api/data", (req, res) => {
+  // Handle POST request here
+  res.send("This is the POST endpoint");
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
